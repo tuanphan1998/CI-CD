f82335e64c8482995a141d8c3032829a50b583c1
TUanphan nam <phantuanw.o.t@gmail.com>
2020-09-24 12:03:45 +0700

    demo cicd

---
+++ precache-manifest.fae37b1d17bc1856f1fe8ad534033582.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a50a6982d818a49ab94fa3a83c235e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "379ddaeee2f21820a897",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b858db5794e8e47b14d2",
+    "url": "/static/js/2.64077007.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64077007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379ddaeee2f21820a897",
+    "url": "/static/js/main.6593b649.chunk.js"
+  },
+  {
+    "revision": "a90f1ea6d88b7a569244",
+    "url": "/static/js/runtime-main.8b8e7235.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
